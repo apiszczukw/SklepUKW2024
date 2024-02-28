2bdf7a6ab31747d996808315acba1c26082cd58b
apiszczukw <apiszczukw@gmail.com>
2024-02-28 18:14:09 +0100

    Organizacja projektu

--- SklepUKW2024/Controllers/HomeController.cs
+++ SklepUKW2024/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace SklepUKW2024.Controllers
             return View();
         }
 
+        public IActionResult Kontakt()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
