216b598967194ebf97a921b54a9c0b39af464570
apiszczukw <apiszczukw@gmail.com>
2024-03-20 18:09:36 +0100

    Strony dynamiczne; widoki cząstkowe

---
+++ SklepUKW2024/Controllers/FilmsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SklepUKW2024.DAL;
+
+namespace SklepUKW2024.Controllers
+{
+	public class FilmsController : Controller
+	{
+		FilmsContext db;
+
+		public FilmsController(FilmsContext db)
+		{
+			this.db = db;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult FilmsList(string categoryName)
+		{
+			var category = db.Categories.Include("Films").Where(c => c.Name.ToUpper() == categoryName.ToUpper()).Single();
+
+			var filmsFromCategory = category.Films.ToList();
+
+			return View(filmsFromCategory);
+		}
+	}
+}
---
+++ SklepUKW2024/ViewComponents/MenuViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SklepUKW2024.DAL;
+
+namespace SklepUKW2024.ViewComponents
+{
+	public class MenuViewComponent : ViewComponent
+	{
+		FilmsContext db;
+
+		public MenuViewComponent(FilmsContext db)
+		{
+			this.db = db;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var categories = db.Categories.ToList();
+
+			return await Task.FromResult((IViewComponentResult)View("_Menu", categories));
+		}
+	}
+}
